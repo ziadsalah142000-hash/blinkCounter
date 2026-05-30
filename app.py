2e800b27e0f8ad8b3eaffac33b3f9a1e69f96418
ziadsalah142000-hash <ziadsalah142000@gmail.com>
2026-05-30 19:00:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Graduation Project Running Successfully"
 
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
